147bf7926a8fb846b12d86968dc40e663b68d225
WinfxkAtLexy <by@winfxk.cn>
2025-07-16 13:15:55 +0800

    update

--- src/cn/winfxk/lexy/wms/im/complete/row/storeissue/Rowstoreissue.kt
+++ src/cn/winfxk/lexy/wms/im/complete/row/storeissue/Rowstoreissue.kt
@@ -4,7 +4,7 @@ import cn.winfxk.lexy.wms.im.client.test.CwsCecp980CreateRequestCwsCecp980Create
 import cn.winfxk.lexy.wms.im.client.test.TIPTOPServiceGateWay
 import cn.winfxk.lexy.wms.im.item.PostLog
 import cn.winfxk.libk.log.Log
-import cn.winfxk.libk.tool.Tool
+import cn.winfxk.libk.tool.code.SnowflakeID
 import cn.winfxk.libk.tool.utils.toJson
 import com.alibaba.fastjson.JSONObject
 import java.io.PrintWriter
@@ -15,58 +15,59 @@ import java.util.*
 /**
  * 原材料发料
  * @param acc 账套
- * @param tc_wsg01 源工单号
- * @param tc_wsg02 工单号
- * @param tc_wsg03 类型: 1发料 2成品入库 3退料
- * @param tc_wsg04 人员（KC号）
- * @param tc_wsg05 部门
- * @param tc_wsg11 料号
- * @param tc_wsg12 仓库
- * @param tc_wsg13 储位
- * @param tc_wsg14 批号
- * @param tc_wsg15 单位
- * @param tc_wsg16 数量
+ * @param tcWsg01 源工单号
+ * @param tcWsg02 工单号
+ * @param tcWsg03 类型: 1发料 2成品入库 3退料
+ * @param tcWsg04 人员（KC号）
+ * @param tcWsg05 部门
+ * @param tcWsg11 料号
+ * @param tcWsg12 仓库
+ * @param tcWsg13 储位
+ * @param tcWsg14 批号
+ * @param tcWsg15 单位
+ * @param tcWsg16 数量
  */
 class Rowstoreissue(
     val acc: String,
-    val tc_wsg01: String,
-    val tc_wsg02: String,
-    val tc_wsg03: String = "1",
-    val tc_wsg04: String,
-    val tc_wsg05: String,
-    val tc_wsg11: String,
-    val tc_wsg12: String,
-    val tc_wsg13: String,
-    val tc_wsg14: String,
-    val tc_wsg15: String,
-    val tc_wsg16: String,
+    val tcWsg01: String,
+    val tcWsg02: String,
+    val tcWsg03: String = "1",
+    val tcWsg04: String,
+    val tcWsg05: String,
+    val tcWsg11: String,
+    val tcWsg12: String,
+    val tcWsg13: String,
+    val tcWsg14: String,
+    val tcWsg15: String,
+    val tcWsg16: String,
 ) {
     companion object {
         private val sfYMDHMS by lazy { SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); }
         private val service by lazy { TIPTOPServiceGateWay() };
         private val port by lazy { service.tiptopServiceGateWayPortType };
+        private val id by lazy { SnowflakeID(1, 1) }
         @JvmStatic
         fun main(args: Array<String>) {
-            sendMessage("KC04",
-                """
-                    {
-                    "tc_wsg01": "61500-250500608",
-                    "tc_wsg02": "61500-250500655",
-                    "tc_wsg03": "1",
-                    "tc_wsg04": "kcl3360",
-                    "tc_wsg05": "6000",
-                    "tc_wsg11": "SEA03801D13R",
-                    "tc_wsg12": "2034",
-                    "tc_wsg13": "",
-                    "tc_wsg14": "",
-                    "tc_wsg15": "PCS",
-                    "tc_wsg16": "1"
-                    }
-                """.trimIndent().toJson()
-            ).apply {
-                println(isSuccess)
-                println(description)
-            }
+            /*            sendMessage("KC04",
+                            """
+                                {
+                                "tc_wsg01": "61500-250500608",
+                                "tc_wsg02": "61500-250500655",
+                                "tc_wsg03": "1",
+                                "tc_wsg04": "kcl3360",
+                                "tc_wsg05": "6000",
+                                "tc_wsg11": "SEA03801D13R",
+                                "tc_wsg12": "2034",
+                                "tc_wsg13": "",
+                                "tc_wsg14": "",
+                                "tc_wsg15": "PCS",
+                                "tc_wsg16": "1"
+                                }
+                            """.trimIndent().toJson()
+                        ).apply {
+                            println(isSuccess)
+                            println(description)
+                        }*/
         }
         @JvmStatic
         fun sendMessage(acc: String, json: com.alibaba.fastjson2.JSONObject) = sendMessage(acc, JSONObject(json));
@@ -95,12 +96,20 @@ class Rowstoreissue(
 
     private val log = Log(this.javaClass.simpleName)
     private val date = Date();
-    val reqno: String by lazy {
-        val time = System.currentTimeMillis();
-        val random = Random();
-        return@lazy "${Tool.CompressNumber(time)}-${Tool.CompressNumber(random.nextInt(9999999).toLong())}"
+    val reqno: String by lazy { id.nextKey() }
+    private val plog by lazy {
+        val log = PostLog();
+        log.IPC = UUID.randomUUID().toString();
+        log.SYS_C = "WMS";
+        log.ITEM_C = "注塑发货";
+        log.ITEM_N = "注塑件收货";
+        log.STATE = "NG";
+        log.CONTEXT = xml;
+        log.CNAME = tcWsg04
+        log.FAC_C = tcWsg05;
+        log.MCODE = "1001";
+        return@lazy log
     }
-
     val xml: String by lazy {
         """
         <Request>
@@ -129,15 +138,15 @@ class Rowstoreissue(
                     <RecordSet id="1">
                         <Master name="header">
                             <Record>
-                                <Field name="tc_wsg01" value="$tc_wsg01"/>
+                                <Field name="tc_wsg01" value="$tcWsg01"/>
                                 --varchar2(20) 源工单
-                                <Field name="tc_wsg02" value="$tc_wsg02"/>
+                                <Field name="tc_wsg02" value="$tcWsg02"/>
                                 --varchar2(20) 当前工单
-                                <Field name="tc_wsg03" value="$tc_wsg03"/>
+                                <Field name="tc_wsg03" value="$tcWsg03"/>
                                 --类型: 1发料 2成品入库 3退料
-                                <Field name="tc_wsg04" value="$tc_wsg04"/>
+                                <Field name="tc_wsg04" value="$tcWsg04"/>
                                 --人员
-                                <Field name="tc_wsg05" value="$tc_wsg05"/>
+                                <Field name="tc_wsg05" value="$tcWsg05"/>
                                 --部门
                                 <Field name="tc_wsg06" value="$acc"/>
                                 --当前法人
@@ -145,17 +154,17 @@ class Rowstoreissue(
                         </Master>
                         <Detail name="body">
                             <Record>
-                                <Field name="tc_wsg11" value="$tc_wsg11"/>
+                                <Field name="tc_wsg11" value="$tcWsg11"/>
                                 --varchar2(40) 料号
-                                <Field name="tc_wsg12" value="$tc_wsg12"/>
+                                <Field name="tc_wsg12" value="$tcWsg12"/>
                                 --varchar2(10) 仓库
-                                <Field name="tc_wsg13" value="$tc_wsg13"/>
+                                <Field name="tc_wsg13" value="$tcWsg13"/>
                                 --varchar2(10) 储位
-                                <Field name="tc_wsg14" value="$tc_wsg14"/>
+                                <Field name="tc_wsg14" value="$tcWsg14"/>
                                 --varchar2(40) 批号
-                                <Field name="tc_wsg15" value="$tc_wsg15"/>
+                                <Field name="tc_wsg15" value="$tcWsg15"/>
                                 --varchar2(10) 单位
-                                <Field name="tc_wsg16" value="$tc_wsg16"/>
+                                <Field name="tc_wsg16" value="$tcWsg16"/>
                                 --number(15,3) 数量
                             </Record>
                         </Detail>
@@ -173,37 +182,17 @@ class Rowstoreissue(
             }).apply {
                 return Result(this@Rowstoreissue, response).apply {
                     if (isSuccess) return@apply;
-                    val log = PostLog();
-                    log.IPC = UUID.randomUUID().toString();
-                    log.SYS_C = "WMS";
-                    log.ITEM_C = "注塑发货";
-                    log.ITEM_N = "注塑件收货";
-                    log.STATE = "NG";
-                    log.CONTEXT = xml;
-                    log.RESULT = response;
-                    log.MARK = description;
-                    log.CNAME = tc_wsg04
-                    log.FAC_C = tc_wsg05;
-                    log.MCODE = "1001";
-                    log.post()
+                    plog.RESULT = response;
+                    plog.MARK = description;
+                    plog.post()
                 }
             }
         } catch (e: Exception) {
-            log.e("请求ERP时出现异常！", e);
-            val log = PostLog();
-            log.IPC = UUID.randomUUID().toString();
-            log.SYS_C = "WMS";
-            log.ITEM_C = "注塑发货";
-            log.ITEM_N = "注塑件收货";
-            log.STATE = "NG";
-            log.CONTEXT = xml;
-            log.RESULT = StringWriter().also { e.printStackTrace(PrintWriter(it)) }.toString()
-            log.MARK = e.message;
-            log.CNAME = tc_wsg04
-            log.FAC_C = tc_wsg05;
-            log.MCODE = "1001";
-            log.throwable = e;
-            log.post()
+            this.log.e("请求ERP时出现异常！", e);
+            plog.RESULT = StringWriter().also { e.printStackTrace(PrintWriter(it)) }.toString()
+            plog.MARK = e.message;
+            plog.throwable = e;
+            plog.post()
             return Result(this, null, e)
         }
     }
